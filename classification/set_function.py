1768f6173dcb0cc3aea8029b86f2820d9e7836fc
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2022-05-14 17:16:28 +0900

    Update set_function.py

--- classification/set_function.py
+++ classification/set_function.py
@@ -10,6 +10,7 @@ from PIL import Image
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set()
 %matplotlib inline
 
 import torch
@@ -25,12 +26,11 @@ from torchinfo import summary
 import warnings
 warnings.filterwarnings('ignore')
 
-
-
-
-
-# 乱数の固定化
-def torch_seed(seed=123):
+def torch_seed(seed=0):
+    """Fixed seed value."""
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
@@ -38,12 +38,10 @@ def torch_seed(seed=123):
 
 def filelist_to_imgs(file_list, rows=3, columns=5, scale=3, order=True):
   """
-  rows*columns枚の画像を表示
-  file_list:パスのリスト
-  rows：行数
-  columns:列数
-  scale：調節用
-  order：True：前から順番通り / False：ランダム抽出
+  display rows*columns images
+  file_list：path_list
+  scale：display scaling value
+  order：path_order True/False
   """
   plt.figure(figsize=(scale*columns, scale*rows))
   if order == True:
@@ -63,10 +61,16 @@ def filelist_to_imgs(file_list, rows=3, columns=5, scale=3, order=True):
     plt.show()
 
 def get_mean_std(resize, path_list):
-  resize = [resize,resize]
+  """
+  resize：int
+  path_list：only train -> train_path_list / all -> train_path_list+valid_path_list
 
+  return：mean, std
+  """
+
+  resize = [resize,resize]
   transform = transforms.Compose([
-                  transforms.ToTensor(),  # テンソルに変換
+                  transforms.ToTensor(),
                   transforms.Resize(resize)
                   ])
 
@@ -81,7 +85,12 @@ def get_mean_std(resize, path_list):
 
   return imgs_mean, imgs_std
 
-def tensor_to_image(image_t):
+def tensor_to_img(image_t):
+  """
+  image_t：torch.Tensor
+  display one image
+  tensor -> display image
+  """
   image = image_t.cpu().detach().numpy().transpose((1,2,0)) # CHW -> HWC
   image = np.clip(image, 0, 1) # 0~1clip
   plt.imshow(image)
@@ -89,12 +98,10 @@ def tensor_to_image(image_t):
 
 def dataset_to_imgs(dataset, rows=3, columns=5, scale=3, order=True):
   """
-  rows*columns枚の画像を表示
-  file_list:パスのリスト
-  rows：行数
-  columns:列数
-  scale：調節用
-  order：True：前から順番通り / False：ランダム抽出
+  display rows*columns images
+  file_list：path_list
+  scale：display scaling value
+  order：dataset_order True/False
   """
   # index を作る。0番目からdatasetの長さ。
   dataset_index = [i for i in range(len(dataset))]
@@ -119,6 +126,9 @@ def dataset_to_imgs(dataset, rows=3, columns=5, scale=3, order=True):
     plt.show()
 
 def show_label_balance(train_dataset, valid_dataset):
+  """
+  confirm dataset has label
+  """
   train_label = [labels for imgs, labels in train_dataset]
   valid_label = [labels for imgs, labels in valid_dataset]
 
@@ -139,32 +149,35 @@ def show_label_balance(train_dataset, valid_dataset):
   plt.show()
 
 def detail_dataloader(dataset, dataloader):
+  """
+  show dataset -> dataloader flow
+  """
   print("dataset: ",len(dataset))
   print("batch_size: ", dataloader.batch_size)
   print("dataloader: ",len(dataloader))
 
-  for images, labels in dataloader:
-    break
-
-  print(images.shape)
-  print(labels.shape)
+  data_iter = iter(dataloader)
+  images, labels = next(data_iter)
+  print("===== extract =====")
+  print("images shape: ", images.shape)
+  print("labels shape: ", labels.shape)
 
 def dataloader_to_imgs(dataloader, net=None, device=None):
   """
-  最初のデータローダーのセットから画像を表示
-  最大表示枚数は50枚
-  classificationを行うモデルを引き渡して予測ラベルも合わせて表示できる
+  display dataloader_imags(max:50）
+  net：classification_model
   """
-  for images, labels in dataloader:
-    break
+  data_iter = iter(dataloader)
+  images, labels = next(data_iter)
 
-  n_size = min(len(images), 50) # 最大でも50枚制限
+  n_size = min(len(images), 50) # n_size: len(images) or 50
 
+  # has classification_model
   if net is not None:
     inputs = images.to(device)
     labels = labels.to(device)
 
-    # 予測計算
+    # predict
     outputs = net(inputs)
     predicted = torch.max(outputs, 1)[1]
 
@@ -184,7 +197,7 @@ def dataloader_to_imgs(dataloader, net=None, device=None):
       # ax.set_title(str(labels[i].item()) + "/" , c=c, fontsize=20)
 
     else:
-      ax.set_title(labels[i].item(), fontsize=20) # .itemでtensor表示を消す
+      ax.set_title(labels[i].item(), fontsize=20) # .item -> delete tensor
     image_np = images[i].numpy().copy()
     img = np.transpose(image_np, (1,2,0))
     img = np.clip(img, 0,1)
@@ -195,27 +208,32 @@ def dataloader_to_imgs(dataloader, net=None, device=None):
   plt.show()
 
 def transform_test(file_list, index=0 ,transform=None):
+  """
+  an image：file_list[index] -> transform
+  display original_image and transformed_image
+  """
   plt.figure(figsize=(8, 8))
   # original_image
   img = Image.open(file_list[index])
   print("original_img: ",np.array(img).shape)
   ax = plt.subplot(1, 2, 1)
+  # plt.axis("off")
   plt.imshow(img)
 
-  # transformed_img
+  # transformed_image
   img_t = transform(img)
   print("transformed_img: ",img_t.shape)
   img_t = img_t.numpy().transpose((1,2,0)) # CHW -> HWC
   img_t = np.clip(img_t, 0, 1) # 0~1clip
   ax = plt.subplot(1, 2, 2)
+  # plt.axis("off")
   plt.imshow(img_t)
 
   plt.show()
 
 def net_test(net, dataset):
   """
-  classification用
-  datasetから回収した画像1枚に対して出力のテストを行う
+  dataset -> an image -> net
   """
   img = dataset[0][0]
   print("get_image: ", img.shape)
@@ -225,8 +243,7 @@ def net_test(net, dataset):
 
 def show_net_status(net, dataloader):
   """
-  パラメータの総数
-  summaryは入力次元を引数に取るので、dataloaderからB,C,H,Wの次元を回収
+  summary input：dataloader's B,C,H,W
   """
   print("net")
   print(net)
@@ -242,17 +259,23 @@ def show_net_status(net, dataloader):
 
 # 重みとバイアスの初期化（完全ランダムよりこっちの方が良い？？）
 def weights_init(m):
-  # m は model
+  """
+  m：model
+  Conv / BatchNorm weights initialization（based normal distribution） 
+  """
   classname = m.__class__.__name__
   # findは Conv という文字列が あれば何文字目か / なければ -1 を返す ので if は Conv がある場合を指定している
   if classname.find("Conv") != -1:
-    nn.init.normal_(m.weight.data, 0.0, 0.02) # 正規分布を使って初期化
+    nn.init.normal_(m.weight.data, 0.0, 0.02) # 正規分布
   
   elif classname.find("BatchNorm") != -1:
     nn.init.normal_(m.weight.data, 1.0, 0.02)
     nn.init.constant_(m.bias.data, 0)
 
 def visualize_process(train_loss_log, valid_loss_log, train_acc_log, valid_acc_log):
+  """
+  4params = def training 's return
+  """
   plt.figure(figsize=(15,5))
   ax = plt.subplot(1, 2, 1)
   plt.plot(train_loss_log, label="train")
