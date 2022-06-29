041663c57558027a3c3f6c43d83c83a50691e98f
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2022-06-29 17:15:06 +0900

    Update classification_utils.py

--- classification/classification_utils.py
+++ classification/classification_utils.py
@@ -270,6 +270,7 @@ def weights_init(m):
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+
 def visualize_process(train_loss_log, valid_loss_log, train_acc_log, valid_acc_log):
     """
     4params = def training 's return
