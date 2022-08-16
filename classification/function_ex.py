b3168a964f766274d8af63d64523550c782c2e26
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2022-08-16 20:09:31 +0900

    Update function_ex.py

--- classification/function_ex.py
+++ classification/function_ex.py
@@ -1,8 +1,8 @@
 def make_pathlist(filepath, RGB_check = True):
     """
         sample
-    rootpath = "/content/drive/MyDrive/0.0_pytorch_baseline/pokemon/train/"
-    filepath = os.path.join(train_rootpath + "**/*.jpg")
+    rootpath = "/content/drive/MyDrive/0.0_pytorch_baseline/pokemon/train"
+    filepath = os.path.join(train_rootpath , "**/*.jpg")
     ** ：class_n folder (n=1~K)
         make_pathlist(filepath)
     RGB_check：eliminate_not3d image（default）
