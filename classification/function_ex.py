4c90057c38bf11602bdad4ae4091b36dda0f97b2
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2022-08-16 20:00:57 +0900

    Update function_ex.py

--- classification/function_ex.py
+++ classification/function_ex.py
@@ -1,19 +1,16 @@
 def make_pathlist(filepath, RGB_check = True):
-  """
-	sample
-  rootpath = "/content/drive/MyDrive/0.0_pytorch_baseline/pokemon/train/"
-  filepath = os.path.join(train_rootpath + "**/*.jpg")
-  ** ：class_n folder (n=1~K)
-	make_pathlist(filepath)
-
-  RGB_check：eliminate_not3d image（default）
-  """
-  # check RGB
-  if RGB_check:
-    path_list = [ path for path in glob.glob(filepath) if Image.open(path).mode == "RGB"]
-    
-  # check RGB = False
-  else:
-    path_list = [ path for path in glob.glob(filepath)]
-
-  return path_list
+    """
+        sample
+    rootpath = "/content/drive/MyDrive/0.0_pytorch_baseline/pokemon/train/"
+    filepath = os.path.join(train_rootpath + "**/*.jpg")
+    ** ：class_n folder (n=1~K)
+        make_pathlist(filepath)
+    RGB_check：eliminate_not3d image（default）
+    """
+    # check RGB
+    if RGB_check:
+        path_list = [ path for path in glob.glob(filepath) if Image.open(path).mode == "RGB"]
+    # check RGB = False
+    else:
+        path_list = [ path for path in glob.glob(filepath)]
+    return path_list
