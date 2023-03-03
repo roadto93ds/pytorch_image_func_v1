d50fc5cd8ae7a0f871bacdc4571c8dc9efda15a2
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2023-03-03 20:16:26 +0900

    Create dataloader_.py

---
+++ my_utils/dataloader_.py
@@ -0,0 +1,34 @@
+### base
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+
+### for classification (imbalanced) / weighted random
+def get_sample_weights(dataset):
+    """
+    sample_weights : assign class weights by dataset
+    len(sample_weights) = len(dataset)
+    """
+    label_list = [data_[1].item() for data_ in dataset]
+    n_class = np.unique(np.array(label_list))
+    count_list = [label_list.count(class_) for class_ in n_class]
+
+    class_weights = list()
+    for count_ in count_list:
+        if count_ > 0:
+            class_weights.append(1/count_)
+
+    sample_weights = [0] * len(dataset)
+    for idx, (data, label) in enumerate(dataset):
+        class_weight = class_weights[label]
+        sample_weights[idx] = class_weight
+    return sample_weights
+
+def get_weighted_sampler(sample_weights, replacement_ = True):
+    """
+    return : WeightedRandom DataLoader
+        oversampling : replacement_ = True
+        not oversampling : replacement_ = False
+    dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler, num_workers=8, pin_memory=True)
+    """
+    sampler = WeightedRandomSampler(sample_weights, num_samples=len(sample_weights), replacement=replacement_)
+    return sampler
