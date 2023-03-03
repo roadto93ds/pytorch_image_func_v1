7aa29138a239fa9e4a30cfd1307c1f0a2d9eefb8
skato0126 <71208385+roadto93ds@users.noreply.github.com>
2023-03-03 18:46:44 +0900

    Create check_.py

---
+++ my_utils/check_.py
@@ -0,0 +1,15 @@
+def pathlist_detail(pathlist:list, label_folder=False):
+    """
+    label_folder = True
+    path ::: data_dir/*** folder ***/img_file.xxx
+    """
+    ext_list = [os.path.splitext(path_)[1] for path_ in pathlist]
+    print(f"pathlist_len = {len(pathlist)}")
+    print(collections.Counter(ext_list))
+    if label_folder:
+        name_list = [path_.split("/")[-2] for path_ in pathlist]
+        print(collections.Counter(name_list))
+
+def df_detail(df):
+    print(df.shape)
+    print(df.columns)
